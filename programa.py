7dd913fcd3d7ef82d4e015f36056727dd09e8bcb
Daniela Nieto <nietogonzalezdaniela@gmail.com>
2024-04-09 15:35:08 -0500

    linspace

--- programa.py
+++ programa.py
@@ -3,9 +3,8 @@ import numpy as np
 
 #Una forma de crear arreglos ndarray es usando una lista
 
-#Usando arange
-miArreglo=np.arange(-10,10)
+#Usando linspace, Hace lo mismo que arange, 
+#pero genera una precision de cuantos numeros 
+#quiero que genere.Numeros flotantes
+miArreglo=np.linspace(0,100,10)
 print(miArreglo)
-miArreglo=miArreglo.reshape((2,10))
-print(miArreglo)
-print(miArreglo.ndim)
