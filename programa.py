58007a9e6de942f1c02b6d2c8eeb48f15ab0a9e1
Daniela Nieto <nietogonzalezdaniela@gmail.com>
2024-04-09 14:59:42 -0500

    listas

--- programa.py
+++ programa.py
@@ -3,15 +3,11 @@ import numpy as np
 
 #Una forma de crear arreglos ndarray es usando una lista
 
-miLista=[3,5,7,9]
-#Arreglo unidimensional
-miArreglo=np.array(miLista)
-#Dimensiones, atributos de ndarray
-print(miArreglo.ndim)
-print(miArreglo.shape)
-print(miArreglo.size)
-print(miArreglo.dtype)
-print(miArreglo.itemsize)
+#Crear arreglos de dos dimensiones apartir de listas
 
-miArreglo2=np.array([3,6,7,90])
+miLista=[(1,3,5,7),(3,8,2,4)],[(3,8,2,4),(1,2,3,4)]
+
+miArreglo=np.array(miLista)
+print(miArreglo.ndim)
+print(miArreglo)
 
