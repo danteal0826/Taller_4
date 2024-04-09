3450b034cde0f965097348257c48b21743f00d4c
Daniela Nieto <nietogonzalezdaniela@gmail.com>
2024-04-09 15:25:29 -0500

    arange

--- programa.py
+++ programa.py
@@ -3,11 +3,9 @@ import numpy as np
 
 #Una forma de crear arreglos ndarray es usando una lista
 
-#Usando funciones de "relleno" de arreglos
-
-miArreglo=np.zeros((5,4))
+#Usando arange
+miArreglo=np.arange(-10,10)
 print(miArreglo)
-miArreglo=np.ones((5,4))
+miArreglo=miArreglo.reshape((2,10))
 print(miArreglo)
-miArreglo=np.empty((5,4))
-print(miArreglo)
+print(miArreglo.ndim)
